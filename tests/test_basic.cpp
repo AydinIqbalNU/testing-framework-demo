a10f4ca8a43da3280111ed1072a13fcf239fdc2c
AydinIqbalNU <aydiniqbal2028@u.northwestern.edu>
2026-06-05 12:46:04 -0500

    Update test_basic.cpp - add failure example test

--- tests/test_basic.cpp
+++ tests/test_basic.cpp
@@ -6,3 +6,8 @@ int add(int a, int b); // too lazy to make harp.h
 TEST(AdditionTest, Basic) {
     EXPECT_EQ(add(2, 3), 5);
 }
+
+// simple test failure example
+TEST(AdditionTest, FailureExample) {
+    EXPECT_EQ(add(2, 3), 6);
+}
