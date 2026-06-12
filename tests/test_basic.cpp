f93a97d546a48712546f9d01554872eb49fafdff
AydinIqbalNU <aydiniqbal2028@u.northwestern.edu>
2026-06-11 22:55:12 -0500

    Update test_basic.cpp

--- tests/test_basic.cpp
+++ tests/test_basic.cpp
@@ -6,8 +6,3 @@ int add(int a, int b); // too lazy to make harp.h
 TEST(AdditionTest, Basic) {
     EXPECT_EQ(add(2, 3), 5);
 }
-
-// simple test failure example
-TEST(AdditionTest, FailureExample) {
-    EXPECT_EQ(add(2, 3), 6);
-}
