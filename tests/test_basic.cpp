8608493fad20904fb02362be3aad764f0589f06d
AydinIqbalNU <aydiniqbal2028@u.northwestern.edu>
2026-06-11 23:07:01 -0500

    Update test_basic.cpp

--- tests/test_basic.cpp
+++ tests/test_basic.cpp
@@ -1,8 +1,13 @@
 #include <gtest/gtest.h>
 
 int add(int a, int b); // too lazy to make harp.h
+int subtract(int a, int b);
 
 // simple test example
 TEST(AdditionTest, Basic) {
     EXPECT_EQ(add(2, 3), 5);
+}
+
+TEST(SubtractionTest, Basic) {
+    EXPECT_EQ(subtract(3,2), 1);
 }
