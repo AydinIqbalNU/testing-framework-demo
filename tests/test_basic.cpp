18aad9512f5ab31f6c0a0612f6da8b3928d80d0c
AydinIqbalNU <aydiniqbal2028@u.northwestern.edu>
2026-06-23 12:42:38 -0500

    Update test_basic.cpp

--- tests/test_basic.cpp
+++ tests/test_basic.cpp
@@ -1,7 +1,11 @@
 #include <gtest/gtest.h>
 #include "../src/math_functions.h"
+#include "../src/more_math/complex_formulas.h"
 
-TEST(math_functions, add)
-{
+TEST(math_functions, add) {
     EXPECT_EQ(add(2, 3), 5);
+}
+
+TEST(complex_formulas, hypotenuse) {
+    EXPECT_EQ(find_hypotenuse(3, 4), 5);
 }
