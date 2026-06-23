2afc5c13c2ae10b0f50c90f26445ac2f17f1074c
AydinIqbalNU <aydiniqbal2028@u.northwestern.edu>
2026-06-23 12:17:09 -0500

    Delete src directory

--- src/external/fontawesome/data/FontAwesomeData.h
+++
@@ -1,8 +0,0 @@
-// IMPORTANT! This file is auto-generated see extras/AwesomeMaker
-
-#pragma once
-
-namespace FontAwesomeData {
-    extern const char*  fontawesomewebfont_ttf;
-    const int           fontawesomewebfont_ttfSize = 165548;
-}
